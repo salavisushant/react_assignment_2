d4571d4853cc50b5d5a46f3327627faf2e245df6
salavisushant <87421852+salavisushant@users.noreply.github.com>
2021-12-23 19:35:18 +0530

    babel

---
+++ reactntro/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div class="align">
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li class="sq">Samsung</li>
+        <li class="sq">HTC</li>
+        <li>Micromax</li>
+        <li class="disc">Apple</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
